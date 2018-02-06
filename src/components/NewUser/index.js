72fdc94a5a14d57cbb81c11b7726524f5da1cdaf
Lauren Wright <lmwright6@crimson.ua.edu>
2018-02-06 17:04:12 -0600

    new user form improvements

--- src/components/NewUser/index.js
+++ src/components/NewUser/index.js
@@ -9,38 +9,49 @@ this.state = {emailInput: ''}
 this.state= {firstNameInput: ''}
 this.state = {lastNameInput: ''}
 this.state = {passwordInput: ''}
-this.state = {reEnterInput:''}
+this.state = {reEnterPasswordInput:''}
 this.handleEmailInput = this.handleEmailInput.bind(this);
+this.handleFirstNameInput = this.handleFirstNameInput.bind(this);
+this.handleLastNameInput = this.handleLastNameInput.bind(this);
+this.handlePasswordInput = this.handlePasswordInput.bind(this);
+this.handleReEnterPasswordInput = this.handleReEnterPasswordInput.bind(this);
 }
 handleEmailInput(event) {
 this.setState({emailInput: event.target.value})
 }
-handlefirstNameInput(event){
+handleFirstNameInput(event){
 this.setState({lastNameInput: event.target.value})
 }
-handlelastNameInput(event){
+handleLastNameInput(event){
 this.setState({lastNameInput: event.target.value})
 }
-handlepasswordInput(event){
+handlePasswordInput(event){
 this.setState({passwordInput: event.target.value})
 }
-handlereEnterInput(event){
-this.setState({reEnterInput: event.target.value})
+handleReEnterPasswordInput(event){
+this.setState({reEnterPasswordInput: event.target.value})
 }
 render() {
 return (
-<div className="App">
-Email: 
-<input className="emailInput" type="text" value={this.state.emailInput} onChange={this.handleEmailInput} />
-FirstName:
-<input className="firstNameInput" type="text" value={this.state.firstNameInput} onChange={this.handlefirstNameInput} />
-LastName:
-<input className="lastNameInput" type="text" value={this.state.lastNameInput} onChange={this.handlelastNameInput} />
-Password:
-<input className="passwordInput" type="text" value={this.state.passwordInput} onChange={this.handlepasswordInput} />
-Re-Enter Password:
-<input className="reEnterInput" type="text" value={this.state.reEnterInput} onChange={this.reEnterInput} />
-</div>
+<form className="NewUser">
+  Email: 
+  <input className="emailInput" type="text" value={this.state.emailInput} onChange={this.handleEmailInput} />
+  <br />
+  FirstName:
+  <input className="firstNameInput" type="text" value={this.state.firstNameInput} onChange={this.handleFirstNameInput} />
+  <br />
+  LastName:
+  <input className="lastNameInput" type="text" value={this.state.lastNameInput} onChange={this.handleLastNameInput} />
+  <br />
+  Password:
+  <input className="passwordInput" type="text" value={this.state.passwordInput} onChange={this.handlePasswordInput} />
+  <br />
+  Re-Enter Password:
+  <input className="reEnterInput" type="text" value={this.state.reEnterPasswordInput} onChange={this.handleReEnterPasswordInput} />
+  <br />
+  <button className="newUserSubmit" type="submit" value="submit">Submit</button>
+
+</form>
 );
 }
 
