fdd0285e5273e4c6144f25e8c36d6a665fa5aeed
matt bell <mattbell2217@gmail.com>
2018-02-06 15:28:59 -0600

    testMatt added a first name input box

--- src/App.js
+++ src/App.js
@@ -6,32 +6,31 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {emailInput: ''}
+    this.state = { emailInput: '' }
+    this.state = { firstNameInput: '' }
+
 
     this.handleEmailInput = this.handleEmailInput.bind(this);
+    this.handleFirstNameInput = this.handleFirstNameInput.bind(this);
   }
+
   handleEmailInput(event) {
-    this.setState({emailInput: event.target.value})
+    this.setState({ emailInput: event.target.value })
   }
+
+  handleFirstNameInput(event) {
+    this.setState({ emailInput: event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
 
-        <body>
-          <input type = "text" name= "email" placeholder = "Enter email"/> <br />
-          <input type = "text" name= "firstname" placeholder = "Enter First Name"/> <br/>
-          <input type = "text" name= "lastname" placeholder = "Enter Last Name"/> <br/>
-          <input type = "text" name= "password" placeholder = "Enter Password"/><br/>
-          <input type = "text" name= "password2" placeholder = "Re-type Password"/><br/>
-          <select>
-            <option value="Band/artist">Band/artist</option>
-            <option value="Venue">Venue</option>
+        Email:
+        <input className = "emailInput" type = "text" value ={this.state.emailInput} onChange = {this.handleEmailInput} /><br/>
 
-          </select>
-        </body>
-        Email: 
-       <input className="emailInput" type="text" value={this.state.emailInput} onChange={this.handleEmailInput}  />
-
+        First Name:
+        <input className = "firstNameInput" type = "text" value = {this.state.firstNameInput} onChange = {this.handleFirstNameInput} />
       </div>
     );
   }
