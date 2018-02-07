8a32cdeed1ca8845dcf69a25e93ebc9c0160820c
matt bell <mattbell2217@gmail.com>
2018-02-07 13:01:14 -0600

    updated the app.js without props

--- src/App.js
+++ src/App.js
@@ -6,39 +6,10 @@ import NewUser from './components/NewUser';
 class App extends React.Component {
 
 
-  constructor(props) {
-    super(props);
-    this.state = { emailInput: '' }
-    this.state = { firstNameInput: '' }
-
-
-    this.handleEmailInput = this.handleEmailInput.bind(this);
-    this.handleFirstNameInput = this.handleFirstNameInput.bind(this);
-  }
-
-  handleEmailInput(event) {
-    this.setState({ emailInput: event.target.value })
-  }
-
-  handleFirstNameInput(event) {
-    this.setState({ emailInput: event.target.value })
-  }
-
   render() {
     return (
       <div className="App">
-
-        Email:
-        <input className = "emailInput" type = "text" value ={this.state.emailInput} onChange = {this.handleEmailInput} /><br/>
-
-        First Name:
-        <input className = "firstNameInput" type = "text" value = {this.state.firstNameInput} onChange = {this.handleFirstNameInput} />
-
-  render() {
-    return (
-      <div className="App">
-       <NewUser/>
->>>>>>> a7555efc5d8bae1ed42b6bb736672b68ce43bcfc
+        <NewUser/>
       </div>
     );
   }
