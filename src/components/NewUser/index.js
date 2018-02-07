1cafaa724f6f80ec899edf13f81690277272ec67
matt bell <mattbell2217@gmail.com>
2018-02-07 13:30:10 -0600

    added a button just writes to the console, rounded the text boxes

--- src/components/NewUser/index.js
+++ src/components/NewUser/index.js
@@ -11,6 +11,9 @@ this.state = {lastNameInput: ''}
 this.state = {passwordInput: ''}
 this.state = {reEnterInput:''}
 this.handleEmailInput = this.handleEmailInput.bind(this);
+
+
+
 }
 handleEmailInput(event) {
 this.setState({emailInput: event.target.value})
@@ -27,19 +30,26 @@ this.setState({passwordInput: event.target.value})
 handlereEnterInput(event){
 this.setState({reEnterInput: event.target.value})
 }
+
+clicked(){
+    console.log('the button was clicked');
+}
+
 render() {
 return (
 <div className="App">
 Email: 
-<input className="emailInput" type="text" value={this.state.emailInput} onChange={this.handleEmailInput} />
+<input className="emailInput" type="text" id="textBox1" value={this.state.emailInput} onChange={this.handleEmailInput} /> <br />
 FirstName:
-<input className="firstNameInput" type="text" value={this.state.firstNameInput} onChange={this.handlefirstNameInput} />
+<input className="firstNameInput" type="text" id="textBox1"  value={this.state.firstNameInput} onChange={this.handlefirstNameInput} /> <br />
 LastName:
-<input className="lastNameInput" type="text" value={this.state.lastNameInput} onChange={this.handlelastNameInput} />
+<input className="lastNameInput" type="text" id="textBox1" value={this.state.lastNameInput} onChange={this.handlelastNameInput} /><br />
 Password:
-<input className="passwordInput" type="text" value={this.state.passwordInput} onChange={this.handlepasswordInput} />
+<input className="passwordInput" type="text" id="textBox1" value={this.state.passwordInput} onChange={this.handlepasswordInput} /> <br />
 Re-Enter Password:
-<input className="reEnterInput" type="text" value={this.state.reEnterInput} onChange={this.reEnterInput} />
+<input className="reEnterInput" type="text" id="textBox1" value={this.state.reEnterInput} onChange={this.reEnterInput} /> <br />
+<button onClick={this.clicked}> Create User</button>
+
 </div>
 );
 }
