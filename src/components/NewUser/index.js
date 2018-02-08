b10e57d84352753491374ba86fa4c59af8894463
matt bell <mattbell2217@gmail.com>
2018-02-08 14:05:20 -0600

    add dropdown menu

--- src/components/NewUser/index.js
+++ src/components/NewUser/index.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 
+
 class NewUser extends React.Component {
 
 constructor(props) {
@@ -31,14 +32,22 @@ this.setState({passwordInput: event.target.value})
 handlereEnterInput(event){
 this.setState({reEnterInput: event.target.value})
 }
+
+
+
 render() {
 return (
 <div className="App"> 
 <input className="emailInput" type="text" placeholder="Email" value={this.state.emailInput} onChange={this.handleEmailInput} /><br />
 <input className="firstNameInput" type="text" placeholder="First Name" value={this.state.firstNameInput} onChange={this.handlefirstNameInput} /><br />
 <input className="lastNameInput" type="text" placeholder="Last Name" value={this.state.lastNameInput} onChange={this.handlelastNameInput} /><br />
-<input className="passwordInput" type="text" placeholder="Paswword" value={this.state.passwordInput} onChange={this.handlepasswordInput} /><br />
+<input className="passwordInput" type="text" placeholder="Password" value={this.state.passwordInput} onChange={this.handlepasswordInput} /><br />
 <input className="reEnterInput" type="text" placeholder="Re-enter Password" value={this.state.reEnterInput} onChange={this.reEnterInput} /><br />
+<select>
+<option value = "band"> Band </option>
+<option value = "Venue"> Venue </option>
+</select> <br />
+<button className="submitButton" type="submit">Create User</button>
 </div>
 );
 }
